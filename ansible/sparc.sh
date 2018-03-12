1ed2c02c0bb276c8b7616d94e7e8e40b2ba0f1d9
saketh-samrat-kodam <32060373+saketh-samrat-kodam@users.noreply.github.com>
2018-03-12 15:04:53 +0530

    Update sparc.sh

--- ansible/sparc.sh
+++ ansible/sparc.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export AWS_ACCESS_KEY_ID=AKIAJLQYXVMQYKYB6WQA
-export AWS_SECRET_ACCESS_KEY=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
 export ANSIBLE_HOSTS=/etc/ansible/ec2.py
 export EC2_INI_PATH=/etc/ansible/ec2.ini 
 python /etc/ansible/script.py
